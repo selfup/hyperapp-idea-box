87b70b6bfd7f506c17150f3daadd3cc0433443d4
selfup <boudinot.regis@yahoo.com>
2017-02-06 19:47:08 -0700

    better styling - move on to search next

--- src/views/IdeaBox.js
+++ src/views/IdeaBox.js
@@ -3,9 +3,11 @@ import Ideas from './Ideas';
 import Inputs from './Inputs';
 
 const view = (model, msg) => (html`
-  <div class="idea-box">
-    <h1>IdeaBox 13</h1>
-    ${Inputs(model, msg)}
+  <div>
+    <div class="idea-box">
+      <h1>IdeaBox 13</h1>
+      ${Inputs(model, msg)}
+    </div>
     ${Ideas(model, msg)}
   </div>
 `);
--- src/views/Ideas.js
+++ src/views/Ideas.js
@@ -1,34 +1,34 @@
 import { html } from 'hyperapp';
 
 const Ideas = (model, msg) => (html`
-  <section>
+  <section class="ideas">
     ${model.ideas.map((idea, index) => html`
-      <article
-        class="idea ${idea.quality}"
-      >
-        <h4>${idea.title}</h4>
+      <article class="idea-box">
+        <h4 class="${idea.quality}">${idea.title}</h4>
         <p>${idea.body}</p>
-        <button
-          key=${index}
-          class="add"
-          onclick=${(_, e) => msg.changeQuality(_, e)}
-        >
-          +
-        </button>
-        <button
-          key=${index}
-          class="sub"
-          onclick=${(_, e) => msg.changeQuality(_, e)}
-        >
-          -
-        </button>
-        <button
-          key=${index}
-          class="delete"
-          onclick=${(_, e) => msg.removeIdea(_, e)}
-        >
-          x
-        </button>
+        <div class="actions">
+          <button
+            key=${index}
+            class="add"
+            onclick=${(_, e) => msg.changeQuality(_, e)}
+          >
+            +
+          </button>
+          <button
+            key=${index}
+            class="sub"
+            onclick=${(_, e) => msg.changeQuality(_, e)}
+          >
+            -
+          </button>
+          <button
+            key=${index}
+            class="delete"
+            onclick=${(_, e) => msg.removeIdea(_, e)}
+          >
+            x
+          </button>
+        </div>
       </article>
     `)}
   </section>
