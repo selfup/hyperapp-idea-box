9ea01e61cb419ff9f11e08deb37d735810834a26
selfup <boudinot.regis@yahoo.com>
2017-02-06 19:52:02 -0700

    built

--- bundle.js
+++ bundle.js
@@ -956,9 +956,11 @@ const App = (model, msg) => (__WEBPACK_IMPORTED_MODULE_0_hyperapp__["html"]`
 
 
 const view = (model, msg) => (__WEBPACK_IMPORTED_MODULE_0_hyperapp__["html"]`
-  <div class="idea-box">
-    <h1>IdeaBox 13</h1>
-    ${__webpack_require__.i(__WEBPACK_IMPORTED_MODULE_2__Inputs__["a" /* default */])(model, msg)}
+  <div>
+    <div class="idea-box">
+      <h1>IdeaBox 13</h1>
+      ${__webpack_require__.i(__WEBPACK_IMPORTED_MODULE_2__Inputs__["a" /* default */])(model, msg)}
+    </div>
     ${__webpack_require__.i(__WEBPACK_IMPORTED_MODULE_1__Ideas__["a" /* default */])(model, msg)}
   </div>
 `);
@@ -976,34 +978,34 @@ const view = (model, msg) => (__WEBPACK_IMPORTED_MODULE_0_hyperapp__["html"]`
 
 
 const Ideas = (model, msg) => (__WEBPACK_IMPORTED_MODULE_0_hyperapp__["html"]`
-  <section>
+  <section class="ideas">
     ${model.ideas.map((idea, index) => __WEBPACK_IMPORTED_MODULE_0_hyperapp__["html"]`
-      <article
-        class="idea ${idea.quality}"
-      >
-        <h4>${idea.title}</h4>
+      <article class="idea-box">
+        <h4 class="${idea.quality}">${idea.title}</h4>
         <p>${idea.body}</p>
-        <button
-          key=${index}
-          class="add"
-          onclick=${(_, e) => msg.changeQuality(_, e)}
-        >
-          +
-        </button>
-        <button
-          key=${index}
-          class="sub"
-          onclick=${(_, e) => msg.changeQuality(_, e)}
-        >
-          -
-        </button>
-        <button
-          key=${index}
-          class="delete"
-          onclick=${(_, e) => msg.removeIdea(_, e)}
-        >
-          x
-        </button>
+        <div class="actions">
+          <button
+            key=${index}
+            class="add"
+            onclick=${(_, e) => msg.changeQuality(_, e)}
+          >
+            +
+          </button>
+          <button
+            key=${index}
+            class="sub"
+            onclick=${(_, e) => msg.changeQuality(_, e)}
+          >
+            -
+          </button>
+          <button
+            key=${index}
+            class="delete"
+            onclick=${(_, e) => msg.removeIdea(_, e)}
+          >
+            x
+          </button>
+        </div>
       </article>
     `)}
   </section>
